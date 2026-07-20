9f5d08f8c0d3960b096be84f7d178fcfa46d021e
dewmini <sithudewmini1@gmail.com>
2026-07-20 12:01:46 +0530

    Initial commit: Maven hello-world project

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+import static com.diogonunes.jcolor.Ansi.colorize;
+import static com.diogonunes.jcolor.Attribute.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( colorize( "Green text",GREEN_TEXT() ) );
+        System.out.println( colorize( "Red on white",RED_TEXT(),WHITE_BACK() ) );
+        System.out.println(" Hello World! ");
+    }
+}
